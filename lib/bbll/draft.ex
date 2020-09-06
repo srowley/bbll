6bf9b7c97278b0b4fc9fa9c31e76684e807f36d6
Steve Rowley <srowley@gmail.com>
2020-09-06 10:50:56 -0500

    add BBLL6 (incomplete)

--- lib/bbll/draft.ex
+++ lib/bbll/draft.ex
@@ -2,7 +2,7 @@ defmodule BBLL.Draft do
 
   @player_data BBLL.Players.from_json()
 
-  def all_leagues, do: ["BBLL1", "BBLL2", "BBLL3", "BBLL4", "BBLL5"]
+  def all_leagues, do: ["BBLL1", "BBLL2", "BBLL3", "BBLL4", "BBLL5", "BBLL6"]
 
   def new_from_json(league_name) do
     league_name <> "_draftresults.json"
